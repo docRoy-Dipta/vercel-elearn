78131140599589db1d8640f0c661f24abb96a159
Roy <rsrdipta1@gmail.com>
2023-09-06 23:37:28 +0600

    new changes

--- eLMS/settings.py
+++ eLMS/settings.py
@@ -155,10 +155,9 @@ SESSION_ENGINE = 'django.contrib.sessions.backends.signed_cookies'
 STATIC_URL = '/static/'
 
 
-# STATICFILES_DIRS = [
-#     BASE_DIR / 'static',
-# ]
-
+STATICFILES_DIRS = [
+    BASE_DIR / 'static',
+]
 
 STATICFILES_DIRS = os.path.join(BASE_DIR, 'static'),
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'static')
